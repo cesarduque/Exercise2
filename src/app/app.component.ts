be7c8548a6bd0ad5c5b3ae057be698a8127ed054
Cesar Duque Gomez <cduque1@yuxiglobal.com>
2018-02-07 18:09:06 -0500

    add file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-	title = 'app';
+	title = 'app-Angular';
 	private _imageUrl: string;
 
 	get imageUrl(): string {
