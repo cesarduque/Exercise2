2dae89e2f8f577ade9b81b2567ee104a7ac72fef
Cesar Duque Gomez <cduque1@yuxiglobal.com>
2018-02-07 18:06:56 -0500

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	private _imageUrl: string;
+
+	get imageUrl(): string {
+		return this._imageUrl;
+	}
+
+	set imageUrl(value: string) {
+		this._imageUrl = value;
+	}
+
+	constructor() {
+		setInterval(() => this.generateRandomFace(), 1000);
+	}
+	generateRandomFace() {
+		const baseUrl = 'https://api.adorable.io/avatars/500/';
+		const urlSuffix = '@adorable.io.png';
+		this.imageUrl = `${baseUrl}${Math.floor(Math.random() * 1000)}${urlSuffix}`;
+	}
 }
